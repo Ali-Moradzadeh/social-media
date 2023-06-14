0dd8041e301137c16d4e6166433e91bbe86c99f7
AliMoradzadeh <moracode.master@gmail.com>
2023-06-14 21:33:42 +0330

    add facebook/twitter/google login

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import json
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +27,7 @@ SECRET_KEY = 'django-insecure-3!p_q#bm&u8q(o9(oaqzj)dxc36i78iq(del@%%2*wd0*g=(90
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysite.com', 'localhost', '127.0.0.1']
 
 
 activate_app = lambda app_name: f"{app_name}.apps.{app_name.capitalize()}Config"
@@ -40,6 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #third apps
+    'social_django',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -106,6 +112,23 @@ AUTH_PASSWORD_VALIDATORS = [
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'account.authentication.EmailAuthBackend',
+    'social_core.backends.facebook.FacebookOAuth2',
+    'social_core.backends.twitter.TwitterOAuth',
+    'social_core.backends.google.GoogleOAuth2',
+]
+
+
+SOCIAL_AUTH_PIPELINE = [
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'account.authentication.create_profile',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
 ]
 
 
@@ -143,3 +166,20 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 #Media Configurations
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+#FaceBook Login Configurations
+#SOCIAL_AUTH_FACEBOOK_KEY = ''
+#SOCIAL_AUTH_FACEBOOK_SECRET = ''
+#SOCIAL_AUTH_FACEBOOK_SCOPE = [] #it is Optionally
+
+#Twitter Login Configurations
+#SOCIAL_AUTH_TWITTER_KEY = '' # Twitter API Key
+#SOCIAL_AUTH_TWITTER_SECRET = '' # Twitter API Secret
+
+
+#Google Login Configurations
+with open("/data/data/com.termux/files/home/sec/social-media/google_auth.json") as conf:
+    google_auth_config = json.load(conf)
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY =  google_auth_config['web']['client_id']  # Google Client ID
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = google_auth_config['web']['client_secret']  # Google Client Secret
